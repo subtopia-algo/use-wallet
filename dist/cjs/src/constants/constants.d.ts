eaa8668d93c61e2fcc017ecac1a2336f9663ca8f
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2023-12-12 00:45:31 +0100

    chore: temp include dist

---
+++ dist/cjs/src/constants/constants.d.ts
@@ -0,0 +1,18 @@
+import { Network } from '../types/node';
+export declare enum PROVIDER_ID {
+    KMD = "kmd",
+    CUSTOM = "custom",
+    PERA = "pera",
+    DAFFI = "daffi",
+    MYALGO = "myalgo",
+    ALGOSIGNER = "algosigner",
+    DEFLY = "defly",
+    EXODUS = "exodus",
+    WALLETCONNECT = "walletconnect",
+    MNEMONIC = "mnemonic",
+    MAGIC = "magic"
+}
+export declare const DEFAULT_NETWORK: Network;
+export declare const DEFAULT_NODE_BASEURL = "https://mainnet-api.algonode.cloud";
+export declare const DEFAULT_NODE_TOKEN = "";
+export declare const DEFAULT_NODE_PORT = "";
